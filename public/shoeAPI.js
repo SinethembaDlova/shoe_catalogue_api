50ef25b23c98400fcecac432dd16d2ad9c0cbe62
SinethembaDlova <dSinethemba@gmail.com>
2017-09-14 17:59:35 +0200

    working with modal

---
+++ public/shoeAPI.js
@@ -0,0 +1,19 @@
+//Get access to my HTML elements
+var addButton = document.querySelector("#addButton");
+var buyButton = document.querySelector("#buyButton");
+
+
+
+
+
+
+
+
+
+//event listener that listens to my HTML elements
+
+addButton.addEventListener('click', function() {
+  $('.small.modal')
+  .modal('show')
+  ;
+})
