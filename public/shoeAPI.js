10d9295675aa7e2f62aa50138b81adb089075dcb
SinethembaDlova <dSinethemba@gmail.com>
2017-09-20 16:52:48 +0200

    sorting my filtering

--- public/shoeAPI.js
+++ public/shoeAPI.js
@@ -3,6 +3,7 @@ var addButton = document.querySelector("#addButton");
 var buyButton = document.querySelector("#buyButton");
 var filterPlace = document.querySelector("#filterPlace");
 var shoeCat = document.querySelector('#shoeCat');
+var filterDiv = document.querySelector('.filterDiv')
 
 //Compliling my shoe template
 var shoeTemplate = document.querySelector("#shoeTemplate").innerHTML;
@@ -26,56 +27,76 @@ var url = 'http://localhost:5000/api/shoes';
 
 //Getting all shoes with AJAX
 function getAllShoes() {
-  $.ajax({
-      url: url,
-      type: "get"
-  }).done(function(data) {
-      console.log(data);
+    $.ajax({
+        url: url,
+        type: "get"
+    }).done(function(data) {
+        //Render my shoes
+        shoeCat.innerHTML = shoeTemplateInstance({
+            shoe: data.data
+        });
+        //avoid duplicate for my dropdown
+        var shoeData = data.data;
+        console.log(data.data);
+        for (var i = 0; i < shoeData.length; i++) {
 
-      //Render my shoes
-      shoeCat.innerHTML = shoeTemplateInstance({
-        shoe: data.data
-      });
-      //avoid duplicate for my dropdown
-      var shoeData = data.data;
-      console.log(data.data);
-      for (var i = 0; i < shoeData.length; i++) {
+            if (brandMap[shoeData.brand] === undefined) {
+                brandMap[shoeData[i].brand] = shoeData[i].brand;
+                brandNames.push(shoeData[i].brand);
+            }
 
-        if (brandMap[shoeData.brand] === undefined) {
-          brandMap[shoeData[i].brand] = shoeData[i].brand;
-          brandNames.push(shoeData[i].brand);
+            if (sizeMap[shoeData[i].size] === undefined) {
+                sizeMap[shoeData[i].size] = shoeData[i].size;
+                sizeNumbers.push(shoeData[i].size);
+            }
         }
 
-        if (sizeMap[shoeData[i].size] === undefined) {
-          sizeMap[shoeData[i].size] = shoeData[i].size;
-          sizeNumbers.push(shoeData[i].size);
-        }
-      }
-      console.log("*****Brands****" + brandNames);
-      console.log(brandNames);
-      console.log("*****Size******" + sizeNumbers);
-
-      // Rendering my filter template
-      var filterResults = filterTemplateInstance({
-          brandFilter: brandNames,
-          sizeFilter: sizeNumbers
-      });
-      filterPlace.innerHTML = filterResults;
-  })
+        // Rendering my filter template
+        var filterResults = filterTemplateInstance({
+            brandFilter: brandNames,
+            sizeFilter: sizeNumbers
+        });
+        filterPlace.innerHTML = filterResults;
+    })
 }
 getAllShoes();
 
 //targeting my HTML from the frontend
-var brandSelect = document.querySelector("#brandSelect");
-var sizeSelect = document.querySelector("#sizeSelect");
+//var brandSelect = document.querySelector("#brandSelect");
+//var sizeSelect = document.querySelector("#sizeSelect");
 
 //my brand and sizes
-var brand = brandSelect.value;
-var size =  sizeSelect.value;
 
 //Filter shoes from the DB
 function filterShoes() {
+    if (brand === "All" && size === "All") {
+        getAllShoes();
+      } else if (brand !== "All") {
+        // AJAX call to render a particular brand
+        $.ajax({
+          url: url + "/brand/" + brand,
+          type: "get"
+        }).done(function(data) {
+          //Render my shoes
+          shoeCat.innerHTML = shoeTemplateInstance({
+            shoe: data.data
+          });
+        })
+      }
 
+    else {
+      // AJAX call to render a particular size
+      $.ajax({
+        url: url + "/size/" + size,
+        type: "get"
+      }).done(function(data) {
+        //Render my shoes
+        shoeCat.innerHTML = shoeTemplateInstance({
+          shoe: data.data
+        });
+      })
+    }
+    }
 }
 
 
@@ -83,5 +104,6 @@ function filterShoes() {
 addButton.addEventListener('click', function() {
     $('.ui.tiny.modal').modal('show');
 });
-brandSelect.addEventListener('change', filterShoes);
-sizeSelect.addEventListener('change',filterShoes);
+filterDiv.addEventListener('change', function(e){
+  console.log("e.target");
+});
